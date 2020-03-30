aae21971b9be19fbc1553984267d9a6982435176
Dirk Grunwald <grunwald@colorado.edu>
2020-03-30 22:01:14 +0000

    better print output for forkprob0

--- forkprob0.c
+++ forkprob0.c
@@ -5,9 +5,16 @@ int main()
 {
     int x = 1;
 
-    if (Fork() == 0)
-	printf("p1: x=%d\n", ++x);  //line:ecf:forkprob0:childfork
-    printf("p2: x=%d\n", --x);      //line:ecf:forkprob0:parentfork
+    int pid = Fork();
+    fprintf(stderr,"I got pid=%d\n", pid);
+    
+    if (pid == 0) {
+        x = x + 1;
+        printf("p1: pid = %d, x=%d\n", pid, x);  //line:ecf:forkprob0:childfork
+    }
+    
+    x = x-1;
+    printf("p2: pid = %d, x=%d\n", pid, x);      //line:ecf:forkprob0:parentfork
     exit(0);
 }
 /* $end forkprob0 */
