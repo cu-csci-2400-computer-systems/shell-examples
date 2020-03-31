b7f5ae12b688ccd7eb8e77cffe2b9e1fa915b1c9
Dirk Grunwald <grunwald@colorado.edu>
2020-03-31 16:43:17 +0000

    add example using sigsegv for chapter 9

---
+++ sigsegv1.c
@@ -0,0 +1,25 @@
+#include "csapp.h"
+#include <string.h>
+
+void handler1(int sig) 
+{
+  fprintf(stderr,"Ow! I got signal \"%s\"(#%d)\n",
+	  strsignal(sig), sig);
+  exit(0);
+}
+
+int main() 
+{
+    int i, n;
+    char buf[MAXBUF];
+    char *ptr;
+
+    if (signal(SIGSEGV, handler1) == SIG_ERR)
+        unix_error("signal error");
+
+    fprintf(stderr,"About to poke main..\n");
+    ptr = (char *) main;
+    *ptr = '\n';
+
+}
+/* $end signal1 */
